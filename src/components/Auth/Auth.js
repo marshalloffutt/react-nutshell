73a18fce308b162b809577cb2d43721eed62c0e3
Marshall Offutt <marshalloffutt@gmail.com>
2019-01-07 19:43:23 -0600

    fixes auth aaaawwwyeeeeeeeah

--- src/components/Auth/Auth.js
+++ src/components/Auth/Auth.js
@@ -1,10 +1,24 @@
 import React from 'react';
 import './Auth.scss';
+import authRequests from '../../helpers/data/authRequests';
 
 class Auth extends React.Component {
+  authenticateUser = (e) => {
+    e.preventDefault();
+    authRequests.authenticate().then(() => {
+      // do something here
+    }).catch(err => console.error('there was an error'));
+  }
+
   render() {
     return (
-      <input className="login-btn" type="image" src="https://www.htps.us/UserFiles/Servers/Server_791028/Templates/login-google.png" alt="login with google"/>
+      <input
+        className="login-btn"
+        type="image"
+        src="https://www.htps.us/UserFiles/Servers/Server_791028/Templates/login-google.png"
+        alt="login with google"
+        onClick={this.authenticateUser}
+      />
     );
   }
 }
