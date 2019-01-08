389d8a593b8ae105ca5d8b8bbdce970ecef30dc2
Marshall Offutt <marshalloffutt@gmail.com>
2019-01-07 19:27:01 -0600

    adds auth

---
+++ src/components/Auth/Auth.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Auth.scss';
+
+class Auth extends React.Component {
+  render() {
+    return (
+      <input className="login-btn" type="image" src="https://www.htps.us/UserFiles/Servers/Server_791028/Templates/login-google.png" alt="login with google"/>
+    );
+  }
+}
+
+export default Auth;
