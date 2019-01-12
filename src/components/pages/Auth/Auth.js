e4a5fc5f9d555c88fa05b599f7bc374428d8c070
Marshall Offutt <marshalloffutt@gmail.com>
2019-01-12 17:42:31 -0600

    console logs new user object

--- src/components/pages/Auth/Auth.js
+++ src/components/pages/Auth/Auth.js
@@ -1,5 +1,5 @@
 import React from 'react';
-// import userRequests from '../../../helpers/data/userRequests';
+import userRequests from '../../../helpers/data/userRequests';
 import authRequests from '../../../helpers/data/authRequests';
 import googleButton from './images/googlebutton.png';
 
@@ -10,7 +10,19 @@ class Auth extends React.Component {
     e.preventDefault();
     authRequests.authenticate()
       .then((results) => {
-        console.log(results);
+        console.log(results); // Magic is results.user.uid
+        userRequests.getUserByUid(results.user.uid)
+          .then((userObject) => {
+            // console.log(userObject); // I see me! But let's delete me... now undefined. Good!
+            if (!userObject) {
+              const newUserObject = {
+                userName: `${results.user.displayName}`,
+                photo: `${results.user.photoURL}`,
+                uid: `${results.user.uid}`,
+              };
+              console.log(newUserObject);
+            }
+          });
         this.props.history.push('/home');
       }).catch(err => console.error('error in authenticating', err));
   }
