99ea6ed73c5941089f28354ccce73776d45009d3
Marshall Offutt <marshalloffutt@gmail.com>
2019-01-12 17:31:49 -0600

    console logs results from auth

--- src/components/pages/Auth/Auth.js
+++ src/components/pages/Auth/Auth.js
@@ -1,5 +1,5 @@
 import React from 'react';
-
+// import userRequests from '../../../helpers/data/userRequests';
 import authRequests from '../../../helpers/data/authRequests';
 import googleButton from './images/googlebutton.png';
 
@@ -8,9 +8,11 @@ import './Auth.scss';
 class Auth extends React.Component {
   authenticateUser = (e) => {
     e.preventDefault();
-    authRequests.authenticate().then(() => {
-      this.props.history.push('/home');
-    }).catch(err => console.error('error in authenticating', err));
+    authRequests.authenticate()
+      .then((results) => {
+        console.log(results);
+        this.props.history.push('/home');
+      }).catch(err => console.error('error in authenticating', err));
   }
 
   render() {
