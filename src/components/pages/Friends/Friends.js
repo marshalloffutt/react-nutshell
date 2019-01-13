1ab6c5b4e240dfc072af0f63119b5d3e03b3f41d
Marshall Offutt <marshalloffutt@gmail.com>
2019-01-12 18:17:06 -0600

    console logs all users and friends, and builds scaffolding

--- src/components/pages/Friends/Friends.js
+++ src/components/pages/Friends/Friends.js
@@ -1,11 +1,42 @@
 import React from 'react';
+import smashRequests from '../../../helpers/data/smashRequests';
 import './Friends.scss';
 
 class Friends extends React.Component {
+  state = {
+    users: [],
+    potentials: [],
+    confirmed: [],
+    pending: [],
+  }
+
+  componentDidMount() {
+    smashRequests.usersAndFriends()
+      .then((results) => {
+        console.log(results);
+      }).catch(err => console.error('error in smash', err));
+  }
+
   render() {
     return (
-      <div className='Friends'>
-        <h2>Friends Component</h2>
+      <div className='Friends container'>
+        <h2>Friends</h2>
+        <div className="container">
+          <div className="row">
+            <div className="col-sm">
+              Potential Friends
+              <ul className="potential"></ul>
+            </div>
+            <div className="col-sm">
+              Pending Requests
+              <ul className="pending"></ul>
+            </div>
+            <div className="col-sm">
+              Friends
+              <ul className="confirmed"></ul>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
